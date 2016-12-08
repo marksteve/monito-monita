d39a52bffd31ac807fc9d2243bf0f24eaf685138
Mark Steve Samson <hello@marksteve.com>
2016-12-08 13:29:05 +1100

    Initial commit

---
+++ src/brunch-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(?!app)/,
+        'app.js': /^app/
+      }
+    },
+    stylesheets: {joinTo: 'app.css'}
+  },
+
+  plugins: {
+    babel: {presets: ['es2015']}
+  },
+
+  overrides: {
+    production: {
+      paths: {public: '../'}
+    }
+  }
+};
